69ce6c20044d54888b4e28a91d33574eb620eee8
findit <53036391+finditDeveloper@users.noreply.github.com>
2020-04-05 07:29:56 +0200

    first commit

--- app/screens/Departments/DepartmentView.js
+++ app/screens/Departments/DepartmentView.js
@@ -2,8 +2,33 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var React = require("react");
 var react_redux_1 = require("react-redux");
+var semantic_ui_react_1 = require("semantic-ui-react");
+var core_1 = require("@material-ui/core");
 exports.DepartmentView = function () {
-    return (React.createElement("div", null, "DepartmentView"));
+    var panes = [
+        {
+            menuItem: { key: "department", icon: "sitemap", content: "Departments" },
+            render: function () { return React.createElement(semantic_ui_react_1.Tab.Pane, null, "Tab 1 Content"); }
+        },
+        {
+            menuItem: { key: "users", icon: "users", content: "Users" },
+            render: function () { return React.createElement(semantic_ui_react_1.Tab.Pane, null, "Tab 2 Content"); }
+        },
+        {
+            menuItem: (React.createElement(semantic_ui_react_1.Menu.Item, { key: "messages" },
+                "Notifications",
+                React.createElement(semantic_ui_react_1.Label, null, "15"))),
+            render: function () { return React.createElement(semantic_ui_react_1.Tab.Pane, null, "Tab 3 Content"); }
+        }
+    ];
+    return (React.createElement("div", { style: { width: "100%", display: "flex" } },
+        React.createElement("div", { style: {
+                width: "80%",
+                padding: 10
+            } },
+            React.createElement(semantic_ui_react_1.Tab, { panes: panes })),
+        React.createElement("div", { style: { width: "20%" } },
+            React.createElement(core_1.Button, { style: { width: "100%" } }, "New Department"))));
 };
 var mapStateToProps = function (state) { return ({}); };
 var mapDispatchToProps = {};
--- app/screens/Departments/DepartmentView.tsx
+++ app/screens/Departments/DepartmentView.tsx
@@ -1,20 +1,351 @@
-import React=require('react')
-import { connect } from 'react-redux'
+import React = require("react");
+import { connect } from "react-redux";
+import { Label, Menu, Tab } from "semantic-ui-react";
+import { Button } from "@material-ui/core";
 
 export const DepartmentView = () => {
-    return (
-        <div>
-            DepartmentView
-        </div>
-    )
-}
+  const panes = [
+    {
+      menuItem: { key: "department", icon: "sitemap", content: "Departments" },
+      render: () => <Tab.Pane>Tab 1 Content</Tab.Pane>
+    },
+    {
+      menuItem: { key: "users", icon: "users", content: "Users" },
+      render: () => <Tab.Pane>Tab 2 Content</Tab.Pane>
+    },
+    {
+      menuItem: (
+        <Menu.Item key="messages">
+          Notifications<Label>15</Label>
+        </Menu.Item>
+      ),
+      render: () => <Tab.Pane>Tab 3 Content</Tab.Pane>
+    }
+  ];
 
-const mapStateToProps = (state) => ({
-    
-})
+  return (
+    <div style={{ width: "100%", display: "flex" }}>
+      <div
+        style={{
+          width: "80%",
+          padding: 10
+        }}
+      >
+        <Tab panes={panes} /> 
 
-const mapDispatchToProps = {
-    
-}
 
-export default connect(mapStateToProps, mapDispatchToProps)(DepartmentView)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+      </div>
+      <div style={{ width: "20%" }}>
+        <Button style={{ width: "100%" }}>New Department</Button>
+      </div>
+    </div>
+  );
+};
+
+const mapStateToProps = state => ({});
+
+const mapDispatchToProps = {};
+
+export default connect(mapStateToProps, mapDispatchToProps)(DepartmentView);
